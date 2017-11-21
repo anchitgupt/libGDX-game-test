2b5e8a7054d21b5b39f76ee6a4e88768e17e28c5
Anchit Gupta <anchitgupt2012@gmail.com>
2017-11-21 14:52:53 +0530

    Initial commit

---
+++ desktop/src/com/ateam/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ateam.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ateam.game.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
